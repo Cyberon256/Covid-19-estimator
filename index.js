0dadb8a2781ce4dfae86ea8e0ff997177d1b7c1b
Cyberon256 <go2ssemakula@gmail.com>
2020-04-13 10:00:52 +0300

    Changed url style for the server log

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use((req, res, next) => {
       let actualTimeDiff = timeDiff[0] + (timeDiff[1] * 1e-9);
 
       const timeStamp = (startTime[0] * 1e9) + startTime[1];
-      //const thisLog = timeStamp + '\t\t' + req.baseUrl + req.path + '\t\t' + 'done in ' + actualTimeDiff.toFixed(4) + ' seconds' + lineBreak;
-      const thisLog = timeStamp + '\t\t' + req.url + '\t\t' + 'done in ' + actualTimeDiff.toFixed(4) + ' seconds' + lineBreak;
+      const thisLog = timeStamp + '\t\t' + req.baseUrl + req.path + '\t\t' + 'done in ' + actualTimeDiff.toFixed(4) + ' seconds' + lineBreak;
       // Write to server.log
       fs.appendFile('server.log', thisLog, (err) => {
         if (err) {
