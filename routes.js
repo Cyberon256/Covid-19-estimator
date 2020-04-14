eb15d9911571bd80d1ce76f34e438e86366f1d2f
Cyberon256 <go2ssemakula@gmail.com>
2020-04-14 19:27:49 +0300

    -

--- routes.js
+++ routes.js
@@ -31,8 +31,7 @@ router.get('/logs', (req, res) => {
       // console.log(err)
     } else {
       // return contents of the file
-      res.type('text');
-      //res.type('text/plain');
+      res.setHeader('Content-Type', 'text/plain');
       res.status(200).send(file);
     }
   });
